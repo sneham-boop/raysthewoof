6ee1ea26be45fafd3fa8bcb36ce9233335333a7d
Sneha Mahajan <sneh.km@gmail.com>
2023-10-09 16:28:11 -0500

    Escape apostrophe.

--- src/app/page.js
+++ src/app/page.js
@@ -6,27 +6,27 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <h3>Est. 2023</h3>
-      {/* <h1>Ray's The Woof!</h1> */}
+      {/* <h1>Ray&apos;s The Woof!</h1> */}
       <Image src={ray} alt="Picture of a triangle" width={500} height={500} />
       <p>
         We pride ourselves in providing excellent care for your furry family
-        when they are away from home. 😊 Welcome to Ray's the Woof! Our founder,
+        when they are away from home. 😊 Welcome to Ray&apos;s the Woof! Our founder,
         Rachel has been working with pets since 2005. We are open 7 days a week
-        and open for business and our hours are 8-10 AM and 5-8 PM. At Ray's The
+        and open for business and our hours are 8-10 AM and 5-8 PM. At Ray&apos;s The
         Woof, I strive to provide a home away from home with a safe, secure,
         fun-filled experience for all our fabulous furfriends. I love what I do
         and I am thankful each and every day.
       </p>
       <p>
         Thank you as always for your continued support in these trying times,
-        from all of us here at Ray's The Woof!! 🤗{" "}
+        from all of us here at Ray&apos;s The Woof!! 🤗{" "}
       </p>
       <p>
         Hours of Operation: We are open Tuesday to Friday 6am to 6pm, closed
-        Sat, Sun, Mon and holidays! At Ray's The Woof, we do more than provide
+        Sat, Sun, Mon and holidays! At Ray&apos;s The Woof, we do more than provide
         pet care - we care about your pet just as we care about our own. Not
         only are we animal lovers, but we also have the experience to provide
-        the highest quality of services. Established in 2023 we are Madison's
+        the highest quality of services. Established in 2023 we are Madison&apos;s
         best place to bring your pup to stay and play. Please read our reviews
         and recommendations on our Google and Facebook listing and see why we
         are number one.We offer dog daycare for dogs of all shapes and sizes and
@@ -45,7 +45,7 @@ export default function Home() {
         updated via text about their progress with pictures a few hours after
         they arrive. Once your dog has completed their assessment day then you
         would just text in to reserve your daycare spot. There are tons of
-        pictures on our social media sites, please take a look you'll get a good
+        pictures on our social media sites, please take a look you&apos;ll get a good
         idea of what we do here all day. Feel free to call anytime, we are happy
         to answer any questions.
       </p>
