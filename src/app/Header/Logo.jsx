875611bfd54fe3bf89c4fbca16b46e8051515352
Sneha Mahajan <sneh.km@gmail.com>
2023-10-10 16:44:09 -0500

    Fix apos escape

--- src/app/Header/Logo.jsx
+++ src/app/Header/Logo.jsx
@@ -1,7 +1,7 @@
 import styles from "./Header.module.scss";
 
 function Logo() {
-  return <div className={styles.logo}>Ray's The Woof</div>;
+  return <div className={styles.logo}>Ray&apos;s The Woof</div>;
 }
 
 export default Logo;
